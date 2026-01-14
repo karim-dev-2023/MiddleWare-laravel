27a2e50b56d0b28c749212b6f198db293c93e582
Karim <karim.ayoub.job@gmail.com>
2026-01-14 14:02:31 +0100

    Access Management

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class ProductController extends Controller
 {
@@ -41,7 +42,7 @@ class ProductController extends Controller
             'name'     => $validated['name'],
             'price'    => $validated['price'],
             'user_id'  => $request->user()->id,
-            'is_public'=> $request->has('is_public'),
+            'is_public' => $request->has('is_public'),
         ]);
 
         return redirect()
@@ -54,6 +55,8 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
+        Gate::authorize('view-product', $product);
+
         return view('products.show', compact('product'));
     }
 
@@ -62,6 +65,8 @@ class ProductController extends Controller
      */
     public function edit(Product $product)
     {
+        Gate::authorize('manage-product', $product);
+
         return view('products.edit', compact('product'));
     }
 
@@ -70,6 +75,7 @@ class ProductController extends Controller
      */
     public function update(Request $request,  Product $product)
     {
+        Gate::authorize('manage-product', $product);
         $validated = $request->validate([
             'name'  => ['required', 'string', 'max:255'],
             'price' => ['required', 'numeric', 'min:0'],
@@ -81,6 +87,7 @@ class ProductController extends Controller
             'is_public' => $request->has('is_public'),
         ]);
 
+
         return redirect()
             ->route('products.index')
             ->with('status', 'Produit mis à jour avec succès.');
@@ -91,7 +98,9 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
+        Gate::authorize('manage-product', $product);
         $product->delete();
+
 
         return redirect()
             ->route('products.index')
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,12 @@ Route::middleware('auth')->group(function () {
 
 Route::middleware('auth')->group(function () {
     Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+    Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+    Route::get('/products/store', [ProductController::class, 'store'])->name('products.store');
+    Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
+    Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+    Route::get('/products/{product}/update', [ProductController::class, 'update'])->name('products.update');
+    Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
 });
 
 
